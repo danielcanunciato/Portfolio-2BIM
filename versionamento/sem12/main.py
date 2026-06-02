9a93dacb2eedb953e3bb359bb3fbf644521c0026
Daniel-C-A-O <00001112519920sp@al.educacao.sp.gov.br>
2026-06-02 14:55:05 -0300

    semana 12 versionamento

---
+++ versionamento/sem12/main.py
@@ -0,0 +1,5 @@
+def somar (a,b):
+    return a+b
+
+def div (a,b):
+    return a/b
---
+++ versionamento/sem12/test_main.py
@@ -0,0 +1,7 @@
+from main import somar,div
+
+def test_somar():
+    assert somar(2,3) == 5
+
+def test_div():
+    assert div(10,2) == 5
